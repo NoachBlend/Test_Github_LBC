9ee139a27d5d507a5fd8c98a350f852b706e6379
NoachBlend <StijnTas@gmail.com>
2020-05-01 13:09:05 +0200

    Make def aftrekken()

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 def optellen(value1, value2):
     return value1+value2
 
+def aftrekken(value1, value2):
+    return value1 - value2
+
 print("Dag van uit Github")
 print('Dag 2 van Github')
 
 print(optellen(5, 4))
+print(aftrekken(9, 8))
